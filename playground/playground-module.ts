ee1fe747830af3b4ae21f94bab57a5886457af83
Iepikhin Oleksii <apagtxiepikhin@lan.apa.at>
2019-02-14 11:32:32 +0100

    Some changes in dependencies.

--- playground/playground-module.ts
+++ playground/playground-module.ts
@@ -1,10 +1,10 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from "@angular/forms";
-import {GenticsUICoreModule, OverlayHostService} from "gentics-ui-core";
+import {FormsModule} from '@angular/forms';
+import {GenticsUICoreModule, OverlayHostService} from 'gentics-ui-core';
 
 import {GenticsUIImageEditorModule} from '../dist/index.js';
-import {PlaygroundAppComponent} from "./playground.component";
+import {PlaygroundAppComponent} from './playground.component';
 
 declare const require: any;
 require('./styles.scss');
